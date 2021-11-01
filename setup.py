e907b72e177a097158c48059d9b025c2d0101146
Bill Ritz <billritz@gmail.com>
2021-11-01 15:33:45 -0400

    Fixes to spacing

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'pywerskewl',
-  packages = ['pywerskewl'],
-  version = '1.0',
-  description = 'A python wrapper for the PowerSchool API.',
-  author = 'Bill Ritz',
-  author_email = 'billritz@gmail.com',
-  url = 'https://github.com/billritz/pywerskewl',
-  download_url = 'https://github.com/billritz/pywerskewl/archive/1.0.tar.gz',
-  keywords = ['powerschool'], # arbitrary keywords
-  classifiers = [],
+    name='pywerskewl',
+    packages=['pywerskewl'],
+    version='1.0',
+    description='A python wrapper for the PowerSchool API.',
+    author='Bill Ritz',
+    author_email='billritz@gmail.com',
+    url='https://github.com/billritz/pywerskewl',
+    download_url='https://github.com/billritz/pywerskewl/archive/1.0.tar.gz',
+    keywords=['powerschool'],  # arbitrary keywords
+    classifiers=[],
 )
